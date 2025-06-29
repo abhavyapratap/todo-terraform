b38af0d6d53f9b1034c2fe2394610fc062e3427c
Bhavya pratap singh <bhavyasingh801@gmail.com>
2025-06-29 13:49:35 +0530

    Adding Commits

---
+++ modules/azurerm_nsg/variable.tf
@@ -0,0 +1,5 @@
+variable "nsg_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "security_rule_name" {}
+variable "destination_port_ranges" {}
---
+++ modules/azurerm_vm/vm.tf
@@ -0,0 +1,26 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.size
+  computer_name       = var.computer_name
+  admin_username      = data.azurerm_key_vault_secret.username.value
+  admin_password      = data.azurerm_key_vault_secret.pwd.value
+  disable_password_authentication = false
+  network_interface_ids = [var.network_interface_ids]
+
+  os_disk {
+    name = var.os_disk_name
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.version1
+  }
+
+  custom_data = var.custom_data
+}
